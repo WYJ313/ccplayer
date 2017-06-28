0c405277a70cf86ad25ae9a9a9c6895eff2f55c8
WYJ313 <710409190@qq.com>
2017-06-28 16:32:36 +0800

    通过designer将界面与逻辑分开
    重新设计了界面，修复了一些因此而产生的bug，增加了按钮的图标，增加了精简模式

---
+++ ccplayer/ui/lyricparser.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+import re
+
+class LyricParser:
+    def __init__(self):
+        self.res = {}
+
+    def loads(self, f,):
+        with open(f, 'r') as input:
+            for line in input.readlines():
+                tmp = line.decode('utf-8')
+                #print(tmp)
+                m = re.match('(\[\d{2}:\d{2}\.\d{2}\])(.+)', line)
+                if m is not None:
+                    tmp = m.group()
+                    i = tmp.find('.')
+                    key = tmp[1: i].strip()
+                    j = tmp.find(']')
+                    value = tmp[j+1:].strip()
+                    self.res[key] = value
+
+    def dumps(self):
+        return self.res
+
+
+if __name__ == '__main__':
+    lp = LyricParser()
+    f = u'./musics/晴天.lrc'
+    lp.loads(f)
+    for key in lp.dumps():
+        print(key)
+        print(lp.dumps()[key].decode('utf-8'))
